d7b44c421239866d7bb36319e946729f7f51a135
Alexander <moshenin@inbox.ru>
2023-10-13 14:00:32 +0300

    Refactor app

--- global.mjs
+++
@@ -1,1 +0,0 @@
-export const token = '6438161093:AAGtEPPHA68o16LKclkTl9PTNn7EM8h2c6o';
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import TelegramBot from 'node-telegram-bot-api/lib/telegram.js';
 
+const token = '6438161093:AAGtEPPHA68o16LKclkTl9PTNn7EM8h2c6o';
+
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.chat.text;
+
+  bot.sendMessage(chatId, 'Ответ на сообщение')
 })
