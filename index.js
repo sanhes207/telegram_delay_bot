276b0fc25b9a8855622e737197e4fd92ae8a184f
Alexander <moshenin@inbox.ru>
2023-10-13 14:05:43 +0300

    fix import method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import TelegramBot from 'node-telegram-bot-api/lib/telegram.js';
+const TelegramBot = require('node-telegram-bot-api')
 
 const token = '6438161093:AAGtEPPHA68o16LKclkTl9PTNn7EM8h2c6o';
 
