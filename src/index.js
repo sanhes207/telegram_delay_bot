c5104b31b84321c21d2d2c8877210a29bd34071e
Alexander <moshenin@inbox.ru>
2023-10-16 10:09:18 +0300

    Новые зависимости, смена библиотек

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { Telegraf } from "telegraf"
+import { message } from "telegraf/filters"
+import config from "config"
+
+const bot = new Telegraf(config.get('TELEGRAM_BOT_TOKEN'), {
+  handlerTimeout: Infinity
+})
+
+bot.start((ctx) => {
+    ctx.reply('Вас приветствует мой бот, надеюсь работа будет продуктивной')
+})
